951e197e564532697a884411b8da565fa51d3a6b
respectus <muazs786@gmail.com>
2012-01-03 12:46:36 -0800

    First

---
+++ var/localhost/templates_c/default/^%%41^414^4149CF9E%%home.tpl.html.php
@@ -0,0 +1,9 @@
+<?php /* Smarty version 2.6.7, created on 2012-01-01 03:30:11
+         compiled from cms/home.tpl.html */ ?>
+<?php require_once(SMARTY_CORE_DIR . 'core.load_plugins.php');
+smarty_core_load_plugins(array('plugins' => array(array('function', 'get_mod', 'cms/home.tpl.html', 3, false),)), $this); ?>
+<?php $this->_cache_serials['C:/xampp/htdocs/flexycms/../var/localhost/templates_c/default\^%%41^414^4149CF9E%%home.tpl.html.inc'] = '21ede0dc879e331455b176892b60b9ed'; ?>
+<!-- Template: cms/home.tpl.html Start 01/01/2012 03:30:11 --> 
+ <?php if ($this->caching && !$this->_cache_including) { echo '{nocache:21ede0dc879e331455b176892b60b9ed#0}';}echo $this->_plugins['function']['get_mod'][0][0]->get_mod(array('mgr' => 'cms','mod' => 'cms'), $this);if ($this->caching && !$this->_cache_including) { echo '{/nocache:21ede0dc879e331455b176892b60b9ed#0}';}?>
+
+<!-- Template: cms/home.tpl.html End --> 
