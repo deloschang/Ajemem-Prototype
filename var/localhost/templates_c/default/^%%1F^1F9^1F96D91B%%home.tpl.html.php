c88c4f0f121dc9477e769a95a468502a8e5a2775
respectus <muazs786@gmail.com>
2012-06-12 16:41:45 -0700

    Latest Structure DONE, all profile pages will look the same. Now it's time to style it for MVP launch. More advanced styling will be done after launch.

--- var/localhost/templates_c/default/^%%1F^1F9^1F96D91B%%home.tpl.html.php
+++ var/localhost/templates_c/default/^%%1F^1F9^1F96D91B%%home.tpl.html.php
@@ -1,5 +1,5 @@
-<?php /* Smarty version 2.6.7, created on 2012-06-13 01:41:27
+<?php /* Smarty version 2.6.7, created on 2012-06-13 01:41:39
          compiled from user/home.tpl.html */ ?>
 <?php require_once(SMARTY_CORE_DIR . 'core.load_plugins.php');
 smarty_core_load_plugins(array('plugins' => array(array('function', 'get_mod', 'user/home.tpl.html', 2, false),)), $this); ?>
-<?php $this->_cache_serials['C:/xampp/htdocs/flexycms/../var/localhost/templates_c/default\^%%1F^1F9^1F96D91B%%home.tpl.html.inc'] = 'dc05867e2da7beff2a2cbbd0fe613783';  if ($this->caching && !$this->_cache_including) { echo '{nocache:dc05867e2da7beff2a2cbbd0fe613783#0}';}echo $this->_plugins['function']['get_mod'][0][0]->get_mod(array('mod' => 'user','mgr' => 'user'), $this);if ($this->caching && !$this->_cache_including) { echo '{/nocache:dc05867e2da7beff2a2cbbd0fe613783#0}';}?>
+<?php $this->_cache_serials['C:/xampp/htdocs/flexycms/../var/localhost/templates_c/default\^%%1F^1F9^1F96D91B%%home.tpl.html.inc'] = '6e0d01e8d57f95959925621d8aeb8efb';  if ($this->caching && !$this->_cache_including) { echo '{nocache:6e0d01e8d57f95959925621d8aeb8efb#0}';}echo $this->_plugins['function']['get_mod'][0][0]->get_mod(array('mod' => 'user','mgr' => 'user'), $this);if ($this->caching && !$this->_cache_including) { echo '{/nocache:6e0d01e8d57f95959925621d8aeb8efb#0}';}?>
