5d372f074a530050fa8f0c886ca24989c854106d
Delos Chang <lol.i.laugh@gmail.com>
2012-06-22 22:36:35 -0400

    fixed logout closing out

--- var/localhost/templates_c/default/^%%1F^1F9^1F96D91B%%home.tpl.html.php
+++ var/localhost/templates_c/default/^%%1F^1F9^1F96D91B%%home.tpl.html.php
@@ -1,5 +1,5 @@
-<?php /* Smarty version 2.6.7, created on 2012-06-23 04:13:38
+<?php /* Smarty version 2.6.7, created on 2012-06-23 04:36:31
          compiled from user/home.tpl.html */ ?>
 <?php require_once(SMARTY_CORE_DIR . 'core.load_plugins.php');
 smarty_core_load_plugins(array('plugins' => array(array('function', 'get_mod', 'user/home.tpl.html', 2, false),)), $this); ?>
-<?php $this->_cache_serials['C:/xampp/htdocs/flexycms/../var/localhost/templates_c/default\^%%1F^1F9^1F96D91B%%home.tpl.html.inc'] = '94e5071d8f62e91724956182802c16ab';  if ($this->caching && !$this->_cache_including) { echo '{nocache:94e5071d8f62e91724956182802c16ab#0}';}echo $this->_plugins['function']['get_mod'][0][0]->get_mod(array('mod' => 'user','mgr' => 'user'), $this);if ($this->caching && !$this->_cache_including) { echo '{/nocache:94e5071d8f62e91724956182802c16ab#0}';}?>
+<?php $this->_cache_serials['C:/xampp/htdocs/flexycms/../var/localhost/templates_c/default\^%%1F^1F9^1F96D91B%%home.tpl.html.inc'] = '2b67124b0bf68750882f8562bd1995ea';  if ($this->caching && !$this->_cache_including) { echo '{nocache:2b67124b0bf68750882f8562bd1995ea#0}';}echo $this->_plugins['function']['get_mod'][0][0]->get_mod(array('mod' => 'user','mgr' => 'user'), $this);if ($this->caching && !$this->_cache_including) { echo '{/nocache:2b67124b0bf68750882f8562bd1995ea#0}';}?>
--- var/localhost/templates_c/default/^%%2C^2C0^2C056D19%%left_pan.tpl.html.php
+++ var/localhost/templates_c/default/^%%2C^2C0^2C056D19%%left_pan.tpl.html.php
@@ -1,4 +1,4 @@
-<?php /* Smarty version 2.6.7, created on 2012-06-23 04:12:45
+<?php /* Smarty version 2.6.7, created on 2012-06-23 04:33:20
          compiled from user/left_pan.tpl.html */ ?>
 <?php if ($_SESSION['id_user']): ?>
 
