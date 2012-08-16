a5a771f3efad72f84ffbe094e2e61a8d9d2c5ab3
Delos Chang <lol.i.laugh@gmail.com>
2012-08-16 18:35:20 -0400

    tracking without var

--- var/localhost/templates_c/default/^%%31^311^311FAF50%%macromeme.tpl.html.php
+++
@@ -1,6 +0,0 @@
-<?php /* Smarty version 2.6.7, created on 2012-08-17 00:16:54
-         compiled from static/macromeme.tpl.html */ ?>
-
-<!-- Template: static/macromeme.tpl.html Start 17/08/2012 00:16:54 --> 
- 
-<!-- Template: static/macromeme.tpl.html End --> 
