acf5fff0cdc6b433d1b7960992da331c03d52900
Delos Chang <lol.i.laugh@gmail.com>
2012-08-17 15:36:41 -0400

    merged in

--- var/localhost/templates_c/default/%%21^217^21732D93%%user_home.tpl.html.php
+++
@@ -1,6 +0,0 @@
-<?php /* Smarty version 2.6.7, created on 2011-12-17 17:51:44
-         compiled from user/user_home.tpl.html */ ?>
-
-<!-- Template: user/user_home.tpl.html Start 17/12/2011 17:51:44 --> 
- <center><b>User homessss.</b></center>
-<!-- Template: user/user_home.tpl.html End --> 
--- var/localhost/templates_c/default/^%%31^311^311FAF50%%macromeme.tpl.html.php
+++
@@ -1,6 +0,0 @@
-<?php /* Smarty version 2.6.7, created on 2012-08-17 04:51:46
-         compiled from static/macromeme.tpl.html */ ?>
-
-<!-- Template: static/macromeme.tpl.html Start 17/08/2012 04:51:46 --> 
- 
-<!-- Template: static/macromeme.tpl.html End --> 
